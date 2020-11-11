c600124c8b460d41bcc1fd69c7812c5411434652
Olga Kroshkina <olkroshk@microsoft.com>
2020-11-10 21:11:25 -0800

    added build_and_run_on_win.ps1

---
+++ build_and_run_on_win.ps1
@@ -0,0 +1,15 @@
+$curdir = Get-Location
+$out = ".\out"
+If((Test-Path $out))
+{
+    Remove-Item $out -Force -Recurse
+}
+New-Item -ItemType Directory -Force -Path $out
+cd $out 
+cmake ..\
+$msbuildpath = ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin"
+Set-Item -Path Env:Path -Value ($Env:Path + $msbuildpath)
+msbuild ValidateMaaJwt.sln
+cd .\Debug
+.\validatejwt.exe
+cd $curdir
