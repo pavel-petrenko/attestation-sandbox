af48db545cdaeee8e4b45057a2bd8cf58a284959
Olga Kroshkina <olkroshk@microsoft.com>
2020-11-10 22:52:45 -0800

    windows version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+bool is_verbose(const std::vector<std::string>& argvec);
+std::string get_value(int idx, const std::vector<std::string> & argvec, bool verbose, const std::string & argname, const std::string & default_value);
+
+int main(int argc, char* argv[], char* envp[]) {
+    std::cout << std::endl << "I'm alive..." << std::endl;
+
+    std::vector<std::string> argvec(argc, "");
+    for (int count = 0; count < argc; count++)
+        argvec[count] = argv[count];
+    
+    bool verbose = is_verbose(argvec);
+    if (argvec.size() < 2 || argvec[1] == "--help" || argvec[1] == "-h" || (argvec.size() < 3 && verbose)) {
+        std::cout << std::endl;
+        std::cout << "Usage: validate[.exe]    [file..]" << '\t' << "Validate jwt token and return 0 (SUCCESS) or 1 (FAILURE)" << std::endl;
+        std::cout << "   or: validate[.exe] -v [file..]" << '\t' << "Enable verbose mode. Validate jwt token and return 0 (SUCCESS) or 1 (FAILURE)" << std::endl;
+        std::cout << "   or: validate[.exe]    [file..] [mrsigner] [productid] [mrenclave] [securityversion]" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Arguments:" << std::endl;
+        std::cout << '\t' << "-v or --verbose" << '\t' << "Enable verbose mode" << std::endl;
+        std::cout << '\t' << "-h or --help" << '\t' << "Print Help (this message) and exit" << std::endl;
+
+        exit(EXIT_SUCCESS);
+    }
+
+    if (verbose) {
+        std::cout << std::endl << "Arguments for this run:" << std::endl;
+    }
+
+    std::string jwt_file_name = get_value(1, argvec, verbose, "Validating filename", "default_filename");
+    std::string mrsigner = get_value(2, argvec, verbose, "Expected MrSigner", "");
+    std::string prodid = get_value(3, argvec, verbose, "Expected ProductId", "");
+    std::string mrenclave = get_value(4, argvec, verbose, "Expected MrEnclave", "");
+    std::string securityver = get_value(5, argvec, verbose, "Expected SecurityVersion", "");
+
+    // WRITE ME
+
+    return EXIT_SUCCESS;
+}
+
+bool is_verbose(const std::vector<std::string>& argvec) {
+    return (argvec.size() > 1) && (argvec[1].compare("-v") == 0 || argvec[1].compare("--verbose") == 0);
+}
+
+std::string get_value(int idx, const std::vector<std::string>& argvec, bool verbose, const std::string& argname, const std::string& default_value) {
+    std::string result = argvec.size() > (idx + (size_t)verbose) ? argvec[idx + (size_t)verbose] : default_value;
+    if (verbose && !result.empty()) {
+        std::cout << '\t' << argname << '\t' << ":" << '\t' << result << std::endl;
+    }
+    return result;
 }
